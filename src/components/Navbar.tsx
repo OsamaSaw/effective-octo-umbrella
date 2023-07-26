5e14aa93f6a6de3f964bb35ccc60edc3cf09f5fd
Osama <Osama@fitlov.com>
2023-07-26 13:02:31 +0300

    first look

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 
 const Navbar: React.FC = () => {
+    const [isProductsMenuOpen, setProductsMenuOpen] = useState(false);
+
+    const handleProductsMenuToggle = () => {
+        setProductsMenuOpen(!isProductsMenuOpen);
+    };
+
     return (
         <nav className="bg-gray-800">
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16">
                     <div className="flex-shrink-0">
-                        <span className="text-white">Logo</span>
+                        <Link href="/" passHref>
+                            <span className="text-white cursor-pointer">Logo</span>
+                        </Link>
                     </div>
                     <div className="hidden md:block">
                         <div className="ml-10 flex items-baseline space-x-4">
-                            <Link href="/">
-                                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Home</a>
+                            <Link href="/" passHref>
+                <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                  Home
+                </span>
                             </Link>
-                            <Link href="/products">
-                                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Products</a>
-                            </Link>
-                            <Link href="/about">
-                                <a className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">About</a>
+                            <div
+                                className="relative"
+                                onMouseEnter={handleProductsMenuToggle}
+                                onMouseLeave={handleProductsMenuToggle}
+                            >
+                <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                  Products
+                </span>
+                                {isProductsMenuOpen && (
+                                    <div className="absolute z-10 mt-2 py-2 w-48 bg-white rounded-md shadow-lg">
+                                        <Link href="/products/category1" passHref>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+                        Category 1
+                      </span>
+                                        </Link>
+                                        <Link href="/products/category2" passHref>
+                      <span className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 cursor-pointer">
+                        Category 2
+                      </span>
+                                        </Link>
+                                    </div>
+                                )}
+                            </div>
+                            <Link href="/about" passHref>
+                <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium cursor-pointer">
+                  About
+                </span>
                             </Link>
                         </div>
                     </div>
